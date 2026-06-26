b553c0fff3cfe14dd09a99cf236267aeee4ba56b
joel-gomez-ch <jgomezc@unjbg.edu.pe>
2026-06-25 19:38:40 -0500

    Creacion del tipo de dato libro y lectura de registros

--- libros.cpp
+++ libros.cpp
@@ -8,3 +8,30 @@ struct Libro{
     string editorial;
     int anioPublicacion;
 };
+
+int main() {
+    int n;
+    Libro libros[100];
+
+    cout << "Ingrese la cantidad de libros: ";
+    cin >> n;
+    cin.ignore();
+
+    for (int i = 0; i < n; i++) {
+        cout << "\nLibro " << i + 1 << endl;
+        cout << "Titulo: ";
+        getline(cin, libros[i].titulo);
+
+        cout << "Autor: ";
+        getline(cin, libros[i].autor);
+
+        cout << "Editorial: ";
+        getline(cin, libros[i].editorial);
+
+        cout << "Anio de publicación: ";
+        cin >> libros[i].anioPublicacion;
+        cin.ignore();
+    }
+
+    return 0;
+}
