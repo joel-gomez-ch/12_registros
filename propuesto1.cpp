42cb1168651c59caefea6b48c7ee3bcceab5a5de
joel-gomez-ch <jgomezc@unjbg.edu.pe>
2026-06-30 18:47:02 -0500

    Actualizacion de propuesto1: estructura for para la lectura de los n empleados

--- propuesto1.cpp
+++ propuesto1.cpp
@@ -16,5 +16,19 @@ int main() {
     cin >> n;
     cin.ignore();
 
+    for (int i = 0; i < n; i++) {
+        cout << "\nEmpleado " << i + 1 << endl;
+
+        cout << "Nombre: ";
+        getline(cin, empleados[i].nombre);
+
+        cout << "Sexo: ";
+        getline(cin, empleados[i].sexo);
+
+        cout << "Sueldo: ";
+        cin >> empleados[i].sueldo;
+        cin.ignore();
+    }
+
     return 0;
 }
